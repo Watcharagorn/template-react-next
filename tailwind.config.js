75d5a86d8545582cd518d2328c8cce4f3dbd1608
watcharakorn.may <watcharagorn.may@gmail.com>
2022-09-27 02:31:39 +0700

    initial template

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = withMT({
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
