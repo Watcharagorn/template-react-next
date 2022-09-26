6efb79b3780c297fc3a2016cf21a1b99ddad7577
watcharakorn.may <watcharagorn.may@gmail.com>
2022-09-27 03:14:04 +0700

    add ts support

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,14 @@ import { Button } from "@material-tailwind/react";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Button>Button</Button>;
       <Button>Button2</Button>;
       <button className="border border-slate-300 hover:border-slate-400">
         Send email
       </button>
-    </h1>
+    </div>
+
   )
 }
